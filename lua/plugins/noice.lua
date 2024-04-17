45e8c7dc8038994819212f8b6a1d42f7cf8c54dc
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-04-18 03:06:47 +0700

    chore(noice): some config to show better

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,6 +2,7 @@ return {
   {
     -- https://www.lazyvim.org/plugins/ui#noicenvim
     "folke/noice.nvim",
+    enabled = true,
     event = "VeryLazy",
     dependencies = {
       "MunifTanjim/nui.nvim",
@@ -20,32 +21,116 @@ return {
       routes = {
         {
           filter = {
-            event = "msg_show",
             any = {
+              { event = "msg_show" },
+              { event = "msg_showmode" },
               { find = "%d+L, %d+B" },
               { find = "; after #%d+" },
               { find = "; before #%d+" },
+              { find = "written" },
             },
           },
           view = "mini",
         },
       },
       presets = {
-        bottom_search = true,
+        bottom_search = false,
         command_palette = true,
         long_message_to_split = true,
-        inc_rename = true,
+        inc_rename = false,
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = 5,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = "auto",
+          },
+        },
+        popupmenu = {
+          relative = "editor",
+          position = {
+            row = 8,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = 10,
+          },
+          border = {
+            style = "rounded",
+            padding = { 0, 1 },
+          },
+          win_options = {
+            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+          },
+        },
       },
     },
-  -- stylua: ignore
-  keys = {
-    { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-    { "<leader>Nl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-    { "<leader>Nh", function() require("noice").cmd("history") end, desc = "Noice History" },
-    { "<leader>Na", function() require("noice").cmd("all") end, desc = "Noice All" },
-    { "<leader>Nd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
-    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
-    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
-  },
+    keys = {
+      {
+        "<S-Enter>",
+        function()
+          require("noice").redirect(vim.fn.getcmdline())
+        end,
+        mode = "c",
+        desc = "Noice Redirect Cmdline",
+      },
+      {
+        "<leader>nl",
+        function()
+          require("noice").cmd "last"
+        end,
+        desc = "Noice Last Message",
+      },
+      {
+        "<leader>nh",
+        function()
+          require("noice").cmd "history"
+        end,
+        desc = "Noice History",
+      },
+      {
+        "<leader>na",
+        function()
+          require("noice").cmd "all"
+        end,
+        desc = "Noice All",
+      },
+      {
+        "<leader>nd",
+        function()
+          require("noice").cmd "dismiss"
+        end,
+        desc = "Noice Dismiss All",
+      },
+      {
+        "<c-f>",
+        function()
+          if not require("noice.lsp").scroll(4) then
+            return "<c-f>"
+          end
+        end,
+        silent = true,
+        expr = true,
+        desc = "Scroll Forward",
+        mode = { "i", "n", "s" },
+      },
+      {
+        "<c-b>",
+        function()
+          if not require("noice.lsp").scroll(-4) then
+            return "<c-b>"
+          end
+        end,
+        silent = true,
+        expr = true,
+        desc = "Scroll Backward",
+        mode = { "i", "n", "s" },
+      },
+    },
   },
 }
